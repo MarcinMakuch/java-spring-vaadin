a216ad31b156279c0f49af8ed13dd6e3648127a1
marcin <makuchmarcin0@gmail.com>
2022-06-20 23:20:37 +0200

    vaadin try out

--- src/main/java/com/m76/javaspringvaadin/MainView.java
+++ src/main/java/com/m76/javaspringvaadin/MainView.java
@@ -1,12 +1,22 @@
 package com.m76.javaspringvaadin;
 
-import com.vaadin.flow.component.html.H2;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 @Route("")
 public class MainView extends VerticalLayout {
-    public MainView(){
-        add(new H2("Witaj Wojciech"));
+    public MainView() {
+        TextField name = new TextField("Name");
+        Paragraph greeting = new Paragraph("");
+
+        Button button = new Button("Greet", event -> {
+            greeting.setText("Hello " + name.getValue());
+        });
+
+        add(name, button, greeting);
     }
 }
